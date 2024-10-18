ae6a75ce03970619c894ec16229cd4fc5f494d2b
unknown <alfinohasan629@gmail.com>
2024-10-18 18:47:08 +0700

    add a layout for the navbar

--- src/components/layout/NavbarLayout.jsx
+++ src/components/layout/NavbarLayout.jsx
@@ -1,11 +1,45 @@
 const NavbarLayout = () => {
   return (
-    <nav className="flex justify-between bg-slate-800 h-20 flex-wrap">
-      <div className="flex justify-start items-center ">
-        <h1 className="text-2xl font-bold text-white mt-1 ml-10 ">PInos</h1>
-      </div>
-      <div className="flex justify-end gap-5 items-center text-white mr-4"></div>
+    <nav className="flex justify-between bg-blue-100 h-20 flex-wrap">
+      <LayerOne />
+      <LayerTwo />
     </nav>
+  );
+};
+
+const LayerOne = () => {
+  return (
+    <div className="flex justify-start items-center ">
+      <img
+        src="/public/LOGOALPIN.png"
+        alt="logo"
+        width={50}
+        height={25}
+        className="ml-10 rounded-full"
+      />
+      <h1 className="text-2xl font-bold text-white mt-1 ml-5 text-black">
+        PIN Ecommers
+      </h1>
+    </div>
+  );
+};
+
+const LayerTwo = () => {
+  return (
+    <div className="flex justify-end gap-10 items-center text-white mr-20">
+      <img
+        src="/public/self-esteem.png"
+        alt="likes products"
+        width={50}
+        height={25}
+      />
+      <img
+        src="/public/shopping-cart.png"
+        alt="product list"
+        width={45}
+        height={20}
+      />
+    </div>
   );
 };
 
